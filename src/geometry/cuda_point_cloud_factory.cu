13f6b48a54fa5090034396cb0be5354f3434cb7f
Xu Shengsi (uif93498) <uif93498@rbd83z7u.conti.de>
2023-11-24 10:51:59 +0100

    a little change...

--- src/geometry/cuda_point_cloud_factory.cu
+++ src/geometry/cuda_point_cloud_factory.cu
@@ -254,9 +254,11 @@ __global__ static void __kernel_make_pointcloud_Z16_BGR8(
         p.coordinates.z = depth_xyz[2];
 
         const int color_index = 3 * (target_y * width + target_x);
-        p.color.b = float(__ldg(&color_frame_data[color_index + 0])) / 255.0f;
-        p.color.g = float(__ldg(&color_frame_data[color_index + 1])) / 255.0f;
-        p.color.r = float(__ldg(&color_frame_data[color_index + 2])) / 255.0f;
+
+        float scale = 1.0f / 255.0f;
+        p.color.b = float(__ldg(&color_frame_data[color_index + 0])) * scale;
+        p.color.g = float(__ldg(&color_frame_data[color_index + 1])) * scale;
+        p.color.r = float(__ldg(&color_frame_data[color_index + 2])) * scale;
 
         p.property = gca::point_property::inactive;
 
